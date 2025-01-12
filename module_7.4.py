eb36041936806572df4b0d9d291da332b43ec955
Reaper54-Nsk <Tatarenko.a.nsk@gmail.com>
2025-01-12 23:41:08 +0700

    Update module_7.4.py

--- module_7.4.py
+++ module_7.4.py
@@ -1,11 +1,11 @@
-team1_num = 5  # количество участников первой команды (Мастера кода)
-team2_num = 6  # количество участников второй команды (Волшебники данных)
+team1_num = 5
+team2_num = 6
 
-score_1 = 40  # количество задач, решённых командой 1
-score_2 = 42  # количество задач, решённых командой 2
+score_1 = 40
+score_2 = 42
 
-team1_time = 1552.512  # время за которое команда 1 решила задачи
-team2_time = 2153.31451  # время за которое команда 2 решила задачи
+team1_time = 1552.512
+team2_time = 2153.31451
 
 print("В команде Мастера кода участников: %s ! " % team1_num)
 print("Итого сегодня в командах участников: %s и %s !" % (team1_num, team2_num))
@@ -15,7 +15,7 @@ print("Волшебники данных решили задачи за {} с !"
 
 print(f'Команды решили {score_1} и {score_2} задач.')
 
-# исход соревнования
+
 if score_1 > score_2 or score_1 == score_2 and team1_time < team2_time:
     challenge_result = 'Победа команды Мастера кода!'
 elif score_1 < score_2 or score_1 == score_2 and team1_time > team2_time:
@@ -24,7 +24,7 @@ else:
     challenge_result = 'Ничья!'
 print(f'Результат битвы: {challenge_result}')
 
-tasks_total = score_1 + score_2  # количество задач
-time_avg = (team1_time + team2_time) / tasks_total  # среднее время решения
+tasks_total = score_1 + score_2
+time_avg = (team1_time + team2_time) / tasks_total
 print(f'Сегодня было решено {tasks_total} задач,',
       f'в среднем по {time_avg:.1f} секунды на задачу!')
